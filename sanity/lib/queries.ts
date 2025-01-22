ec32ba8da0bf3f05291b133f1c549abe4f78783e
Adnan-Git-Hub <adnanmasih8@gmail.com>
2025-01-22 12:38:29 +0500

    new

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,38 +1,27 @@
-import { defineQuery } from "next-sanity";
-
-export const allproducts = defineQuery(`
-    *[type == "productSchema"]{
-    _id,
-    name,
-    description,
-    price,
-    category,
-    inventory,
-    status,
-    discountPercentage,
-    pricewithoutDiscount,
-    rating,
-    ratingCount,
-    tags,
-    size,
-    "imageUrl" : image.asset->url
-}`)
+import { groq } from 'next-sanity';
 
 
-export const fourpro = defineQuery(`
-    *[type == "productSchema"][0..3]{
-    _id,
-    name,
-    description,
-    price,
-    category,
-    inventory,
-    status,
-    discountPercentage,
-    pricewithoutDiscount,
-    rating,
-    ratingCount,
-    tags,
-    size,
-    "imageUrl" : image.asset->url
-}`)
+export const sixProductsQuery = groq`*[_type == "product"][0..5]`;
+
+export const allProductsQuery = groq`*[_type == "product"]`;
+
+export const jordanNikeProductsQuery = groq`*[_type == "product" && "nike jordan" in tags]`;
+
+export const jordanProductsQuery = groq`*[_type == "product" && "jordan" in tags]`;
+
+export const airProductsQuery = groq`*[_type == "product" && "air" in tags]`;
+
+export const womenProductsQuery = groq`*[_type == "product" && "women" in tags]`;
+
+export const kidsProductsQuery = groq`*[_type == "product" && "kids" in tags]`;
+
+export const newProductsQuery = groq`*[_type == "product" && "new" in tags]`;
+
+export const menProductsQuery = groq`*[_type == "product" && "men" in tags]`;
+
+
+export const bestsellerProductsQuery = groq`*[_type == "product" && "bestseller" in tags]`;
+
+export const bannerQuery = groq`*[_type == 'banner']{
+    ...
+ }|order(_createdAt asc)`;
